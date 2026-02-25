645503221ee4e1ab2fd293664ccfcc72aa666b0e
Daniel Augusto <danielamsouza00@gmail.com>
2026-02-25 14:59:36 -0300

    filtros funcionando

--- Teste Front-End/js/app.js
+++ Teste Front-End/js/app.js
@@ -2,11 +2,11 @@ let alunos = [];
 let cursos = [];
 let turmas = [];
 
-function redenrizarTabela(){
+function redenrizarTabela(listaAunos){
     let corpoTabela = document.getElementById("corpo-tabela");
     corpoTabela.innerHTML = "";
 
-    alunos.forEach(function(aluno){
+    listaAunos.forEach(function(aluno){
         let cursoAluno = cursos.find(function(curso){
             return curso.id === aluno.degreeId;
         });
@@ -37,8 +37,12 @@ async function buscarDadosAlunos(){
 
         console.log("Dados carregados com sucesso!!");
 
-        redenrizarTabela();
+        redenrizarTabela(alunos);
         preencherFiltros();
+
+        document.getElementById("filtro-curso").addEventListener("change", filtrarTabela);
+        document.getElementById("filtro-turma").addEventListener("change", filtrarTabela);
+
     } catch (erro){
         console.error('Erro em buscar os dados:', erro);
     }
@@ -60,5 +64,18 @@ function preencherFiltros(){
         selectTurma.innerHTML += opcao;
     });
 };
+
+function filtrarTabela(){
+    let idCursoSelecionado = document.getElementById("filtro-curso").value;
+    let idTurmaSelecionada = document.getElementById("filtro-turma").value;
+
+    let alunosFiltrados = alunos.filter(function(aluno){
+        let cursoCorrrespondente = (idCursoSelecionado === "todos") || (aluno.degreeId.toString() === idCursoSelecionado);
+        let turmaCorrespondente = (idTurmaSelecionada === "todos") || (aluno.classId.toString() === idTurmaSelecionada);
+        return cursoCorrrespondente && turmaCorrespondente;
+    });
+
+    redenrizarTabela(alunosFiltrados);
+}
 
 buscarDadosAlunos();
